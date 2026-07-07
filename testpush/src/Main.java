6b55dca901171e2e918022a2b1ee36275d9c6490
Duong Hoang Viet <hoangvietduong222@gmail.com>
2026-07-07 21:40:02 +0700

    Banh troi nuoc

---
+++ testpush/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Than em vua trang lai vua tron");
+        System.out.println("Bay noi ba chim voi nuoc non");
+        System.out.println("Ran nat mac dau tay ke nan");
+        System.out.println("Ma em van giu tam log son");
+    }
+}
